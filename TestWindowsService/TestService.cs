0110a36b89af60596781f23d695a089e5fee8f2f
Vladislav <ulqrbl@gmail.com>
2020-11-25 18:40:41 +0300

    Initial commit

---
+++ TestWindowsService/TestService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWindowsService
+{
+    public partial class TestService : ServiceBase
+    {
+        EventLog eventLog;
+        NumReturner returner;
+        private int eventId = 1;
+        public TestService()
+        {
+            InitializeComponent();
+            eventLog = new EventLog();
+            returner = new NumReturner();
+            returner.Number += OnNumReceived;
+        }
+
+        public void OnNumReceived(object sender, int num)
+        {
+            string message = "Number got" + num.ToString();
+            eventLog.WriteEntry(message, EventLogEntryType.Information, eventId++);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string message = "Service started";
+            eventLog.WriteEntry(message, EventLogEntryType.Information, eventId++);
+        }
+
+        protected override void OnStop()
+        {
+            returner.GiveNumber(125);
+        }
+    }
+}
