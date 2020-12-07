01eb7bb28bedd1555a2f8a74412f765e39387563
Vladislav <ulqrbl@gmail.com>
2020-12-07 10:42:55 +0300

    Source code added
    Added source code for NumReturner class and interface

---
+++ source code/INumReturner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public interface INumReturner
+    {
+        void GiveNumber(int num);
+        event EventHandler<int> Number;
+    }
